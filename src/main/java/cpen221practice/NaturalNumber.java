69d4054a52da69ed0da7532e797b417877b8fb71
H6NG <8.h.a.n.g.l.i.u.8@gmail.com>
2025-11-07 02:32:29 -0500

    testcase passed

--- src/main/java/cpen221practice/NaturalNumber.java
+++ src/main/java/cpen221practice/NaturalNumber.java
@@ -2,6 +2,35 @@ package cpen221practice;
 
 class NaturalNumber {
 
+    private final int n;
+
+    public NaturalNumber(int n) {
+
+        this.n = n;
+        if(n<1){
+            throw new IllegalArgumentException("You must supply a natural number (positive integer)");
+        }
+
+    }
+    public Classification getClassification() {
+        int sum=0;
+
+        for (int i = 1; i <= n / 2; i++) {
+            if (n % i == 0) {
+                sum += i;
+            }
+        }
+
+        if(sum == n){
+            return Classification.PERFECT;
+        }
+        else if(sum < n) {
+            return Classification.DEFICIENT;
+        }
+        else{
+            return Classification.ABUNDANT;
+        }
+    }
 
 
 }
